54a0838b362463f997f84bd5d8fe7626da5e502e
MuadzBelajar <muadzbelajar@gmail.com>
2026-01-12 15:08:30 +0800

    update

---
+++ MVCawal/app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php
+
+class About {
+   public function index($nama = 'rajie al-qadria', $pekerjaan = 'Murid')
+   {
+      echo "Halo, nama saya $nama, saya adalah $pekerjaan";
+   }
+   public function page()
+   {
+      echo 'About/page';
+   }
+
+}
+
+
+?>
